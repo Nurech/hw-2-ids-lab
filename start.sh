a12aa41beffc5a04d467f4bae14f0680c4fc69f5
Joosep Parts <joosep.parts@gmail.com>
2023-10-07 06:38:04 +0300

    init

--- start.sh
+++ start.sh
@@ -21,14 +21,6 @@ if [ ! -d "${HOME}/Desktop/suricata_ansible" ]; then
   mkdir -p "${HOME}/Desktop/suricata_ansible"
 fi
 
-# Download run.yaml from gist
-if wget -O "${HOME}/Desktop/suricata_ansible/run.yaml" "https://gist.githubusercontent.com/Nurech/210e0cc7cef972eee32c21ca7c22dd0b/raw/5173b86e1006f075b9d1db4fe47b5e1ca42108d7/run.yaml"; then
-  echo "Successfully downloaded run.yaml"
-else
-  echo "Failed to download run.yaml"
-  exit 1
-fi
-
 # Run Ansible playbook
 if ansible-playbook "${HOME}/Desktop/suricata_ansible/run.yaml"; then
   echo "Ansible playbook ran successfully"
